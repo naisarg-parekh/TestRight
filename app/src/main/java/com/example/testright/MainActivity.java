36cbd8c2c8a2a28a3f476b6ee71ceee500724ff2
naisarg-parekh <naisargparekh@gmail.com>
2022-05-15 16:46:18 +0530

    commit

---
+++ app/src/main/java/com/example/testright/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.testright;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button OpenCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OpenCamera = (Button) findViewById(R.id.OpenCamera);
+
+        OpenCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Camera.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
